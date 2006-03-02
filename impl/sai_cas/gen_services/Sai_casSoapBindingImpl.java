a473166f4d0d106a391b90b58c6f91ef60da9b56
math <math@8c818b55-100c-0410-84f9-a5579b780be3>
2006-03-02 00:49:51 +0000

    the implementation of the axis services...

---
+++ impl/sai_cas/gen_services/Sai_casSoapBindingImpl.java
@@ -0,0 +1,17 @@
+/**
+ * Sai_casSoapBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.3 Oct 05, 2005 (05:23:37 EDT) WSDL2Java emitter.
+ */
+
+package sai_cas.gen_services;
+
+public class Sai_casSoapBindingImpl implements sai_cas.gen_services.MainAxisServices{
+    public void insertCatalogfromURI(java.lang.Object uriCatalog) throws java.rmi.RemoteException {
+    }
+
+    public void insertCatalog(java.lang.String catalogString) throws java.rmi.RemoteException {
+    }
+
+}
