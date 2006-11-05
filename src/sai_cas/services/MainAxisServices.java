bce43c3b24a4696f3ede82d28847dceec35948e9
math <math@8c818b55-100c-0410-84f9-a5579b780be3>
2006-11-05 21:42:08 +0000

    tried to implement the fault handling...

--- src/sai_cas/services/MainAxisServices.java
+++ src/sai_cas/services/MainAxisServices.java
@@ -16,6 +16,7 @@ import javax.xml.bind.JAXBException;
 import java.rmi.RemoteException;
 
 import org.apache.log4j.Logger;
+import org.apache.axis2.AxisFault;
 
 import sai_cas.XMLCatalog;
 import sai_cas.XMLCatalogException;
@@ -26,31 +27,185 @@ import sai_cas.VotableException;
 
 public class MainAxisServices {
 	static Logger logger = Logger.getLogger("sai_cas.MainAxisServices");
-
+	
 	/**
 	 * 
-	 * @param catalogName
-	 * @param tableName
-	 * @return String[][] -- the array of string pairs (indexName, indexDefinition) for given catalog and table
+	 * @param uriCatalog  -- The URI of the catalogue
 	 * @throws Exception
+	 * @return void
 	 */
-	public static String[][] getIndexes(String catalogName, String tableName) throws XFault
+/*	
+	public static void insertCatalogFromURI(String uriCatalog) throws XFault
 	{
+		
 		Connection conn = null;
 		DBInterface dbi = null;
-		String result[][] = null;
+		XMLCatalog xmlc;
+		URI uri = null;
+		try
+		{
+			uri = new URI (uriCatalog);
+		}
+		catch(URISyntaxException e)
+		{
+		}
+		
 		try
 		{
 			conn = DBConnection.getPooledPerUserConnection();
 			dbi = new DBInterface(conn);
-
-
-			result = dbi.getIndexes(catalogName, tableName);
+			xmlc = new XMLCatalog(uri);
+			xmlc.insertDataToDB(dbi);
+		}
+		catch (SQLException e)
+		{
+			logger.error("Got an exception... ", e);
+			DBInterface.close(dbi, conn, false);
+		}
+		catch (XMLCatalogException e)
+		{
+			logger.error("Got an XMLCatalog exception... ", e);
+			DBInterface.close(dbi, conn, false);		
+		}
+		catch (DBException e)
+		{
+			logger.error("Got an DB exception... ", e);
+			DBInterface.close(dbi, conn, false);
+		}			
+		DBInterface.close(dbi, conn);
 		throw new XFault("");
+
+	}
+*.	
+	/**
+	 *
+	 * @param catalogString -- The whole catalogue as a string
+	 * @throws Exception
+	 * @return void
+	 */
+/*	public static void insertCatalog(String catalogString) throws RemoteException
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		XMLCatalog xmlc;
+		
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			xmlc = new XMLCatalog(catalogString);
+			xmlc.insertDataToDB(dbi);
+		}
+		catch (SQLException e)
+		{
+			logger.error("Got an exception... ", e);
+			DBInterface.close(dbi, conn, false);
+			throw new RemoteException(e.getMessage());			
+		}
+		catch (XMLCatalogException e)
+		{
+			logger.error("Got an XMLCatalog exception... ", e);
+			DBInterface.close(dbi, conn, false);		
+			throw new RemoteException(e.getMessage());
+		}
+		catch (DBException e)
+		{
+			logger.error("Got an DB exception... ", e);
+			DBInterface.close(dbi, conn, false);
+			throw new RemoteException(e.getMessage());
+		}
+		DBInterface.close(dbi, conn);
+	}
+
+*/
+	/**
+	 *
+	 * @param catalogString -- The whole catalogue as a string
+	 * @throws Exception
+	 * @return void
+	 */
+/*	public static void insertCatalogFromVotable(String catalogString) throws RemoteException
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		Votable vot;
+		
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			vot = new Votable(catalogString);
+			vot.insertDataToDB(dbi);
+		}
+		catch (SQLException e)
+		{
+			logger.error("Got an exception... ", e);
+			DBInterface.close(dbi, conn, false);
+			throw new RemoteException(e.getMessage());			
+		}
+		catch (VotableException e)
+		{
+			logger.error("Got an Votable exception... ", e);
+			DBInterface.close(dbi, conn, false);		
+			throw new RemoteException(e.getMessage());
+		}
+		catch (DBException e)
+		{
+			logger.error("Got an DB exception... ", e);
+			DBInterface.close(dbi, conn, false);
+			throw new RemoteException(e.getMessage());
+		}
+		DBInterface.close(dbi, conn);
+	}
+
+
+*/	
+	/**
+	 * 
+	 * @return String[] -- the array of catalogues in the system
+	 * @throws Exception
+	 */
+	public static String[] getCatalogNames() //throws RemoteException
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String []result = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getCatalogNames();
 		}
 		catch(SQLException e)
 		{
-			logger.error("Caught an exception... ",e);
+			logger.error("Caught an exception... ", e);
+			DBInterface.close(dbi, conn);
+			//throw new RemoteException(e.getMessage());
+		}
+		DBInterface.close(dbi, conn);
+		return result;
+	}
+
+
+	/**
+	 * 
+	 * @return String -- the info about the catalogue
+	 * @throws Exception
+	 */
+	public static String getCatalogInfo(String catalog) 
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String result = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getCatalogInfo(catalog);
+		}
+		catch(SQLException e)
+		{
+			logger.error("Caught an exception... ", e);			
 		}
 		finally
 		{
@@ -59,6 +214,288 @@ public class MainAxisServices {
 		return result;
 	}
 
+	/**
+	 * 
+	 * @return String -- the Description of the catalogue
+	 * @throws Exception
+	 */
+	public static String getCatalogDescription(String catalog) 
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String result = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getCatalogDescription(catalog);
+		}
+		catch(SQLException e)
+		{
+			logger.error("Caught an exception... ", e);			
+		}
+		finally
+		{
+			DBInterface.close(dbi, conn);
+		}
+		return result;
+	}
+
+
+	/**
+	 * 
+	 * @param catalogName
+	 * @return String[] the array of table names in the catalogue
+	 * @throws Exception
+	 */
+	public static String[] getTableNames(String catalogName) //throws Exception
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String result[] = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getTableNames(catalogName);
+		}
+		catch(SQLException e)
+		{
+			logger.error("Caught an exception... ", e);			
+		}
+		finally
+		{
+			DBInterface.close(dbi, conn);
+		}
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param catalogName
+	 * @param tableName
+	 * @return String[] the array of column names in the table of the catalogue
+	 * @throws Exception
+	 */
+	public static String[] getColumnNames(String catalogName, String tableName)// throws Exception
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String result[] = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getColumnNames(catalogName,tableName);
+		}
+		catch(SQLException e)
+		{
+			logger.error("Caught an exception... ", e);			
+		}
+		finally
+		{
+			DBInterface.close(dbi, conn);
+		}
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param catalogName
+	 * @param tableName
+	 * @return String[] the array of infos about the columns in the table of the catalogue
+	 * @throws Exception
+	 */
+	public static String[] getColumnInfos(String catalogName, String tableName) //throws Exception
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String result[] = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getColumnInfos(catalogName,tableName);
+		}
+		catch(SQLException e)
+		{
+			logger.error("Caught an exception... ", e);			
+		}
+		finally
+		{
+			DBInterface.close(dbi, conn);
+		}
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param catalogName
+	 * @param tableName
+	 * @return String[] the array of descriptions of the columns in the table of the catalogue
+	 * @throws Exception
+	 */
+	public static String[] getColumnDescriptions(String catalogName, String tableName)// throws Exception
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String result[] = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getColumnDescriptions(catalogName,tableName);
+		}
+		catch(SQLException e)
+		{
+			logger.error("Caught an exception... ", e);			
+		}
+		finally
+		{
+			DBInterface.close(dbi, conn);
+		}
+		return result;
+	}
+
+	/**
+	 * 
+	 * @param catalogName
+	 * @param tableName
+	 * @return String[] -- the array of units of columns in the table of the catalogue
+	 * @throws Exception
+	 */
+	public static String[] getColumnUnits(String catalogName, String tableName)// throws Exception
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String result[] = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getColumnUnits(catalogName,tableName);
+		}
+		catch(SQLException e)
+		{
+			logger.error("Caught an exception... ", e);			
+		}
+		finally
+		{
+			DBInterface.close(dbi, conn);
+		}
+		return result;
+	}
+
+
+
+
+	/**
+	 * 
+	 * @param catalogName
+	 * @param tableName
+	 * @return String[] -- the array of UCD's of columns in the table of the catalogue
+	 * @throws Exception
+	 */
+	public static String[] getColumnUCDs(String catalogName, String tableName) throws SAICASFault
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String result[] = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getColumnUCDs(catalogName,tableName);
+		}
+		catch(SQLException e)
+		{
+			logger.error("Caught an exception... ", e);
+			throw new SAICASFault("SQLE");
+		}
+		finally
+		{
+			DBInterface.close(dbi, conn);
+		}
+		return result;
+	}
+
+
+	/**
+	 * 
+	 * @param catalogName
+	 * @param tableName
+	 * @return String[][] -- the array of string pairs (indexName, indexDefinition) for given catalog and table
+	 * @throws Exception
+	 */
+	public static String[][] getIndexes(String catalogName, String tableName) throws SAICASFault
+	{
+		Connection conn = null;
+		DBInterface dbi = null;
+		String result[][] = null;
+		try
+		{
+			conn = DBConnection.getPooledPerUserConnection();
+			dbi = new DBInterface(conn);
+			result = dbi.getIndexes(catalogName, tableName);
+		}
+		catch(SQLException e)
+		{
+			logger.error("Caught an exception... ",e);
+			throw new SAICASFault("SQLE",e.getMessage());
+		}
+		finally
+		{
+			DBInterface.close(dbi, conn);
+		}
+		return result;
+	}
+
+
+
+	public static String getConeSearchAsString(String cat, String tab,
+		double ra, double dec, double sr, String format, int verbosity) 
+	{	
+		StringBuffer sb;
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ConeSearch cs = new ConeSearch(pw, format);
+		if (cs.initConeSearch(cat, tab, ra, dec, sr))
+		{
+			cs.setVerbosity(verbosity,false);
+			cs.printConeSearch();
+		}
+		return sw.toString();	
+	}	
+
+	public static String getConeSearchAsString1(String cat, String tab,
+		double ra, double dec, double sr, String format) 
+	{	
+		StringBuffer sb;
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ConeSearch cs = new ConeSearch(pw, format);
+		if (cs.initConeSearch(cat, tab, ra, dec, sr))
+		{
+			cs.setVerbosity(3,false);
+			cs.printConeSearch();
+		}
+		return sw.toString();	
+	}	
+
+	public static String getConeSearchAsString2(String cat, String tab,
+		double ra, double dec, double sr, String format, String columnList[] ) 
+	{	
+		StringBuffer sb;
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ConeSearch cs = new ConeSearch(pw, format);
+		if (cs.initConeSearch(cat, tab, ra, dec, sr))
+		{
+			cs.setColumnList(columnList,false);
+			cs.printConeSearch();
+		}
+		return sw.toString();	
+	}	
 
 	
 	public static Calendar getDBLastChangedDate()
