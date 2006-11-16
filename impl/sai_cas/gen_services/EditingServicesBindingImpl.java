c57877b4c4bd950f18b2a799dac9a26d73401486
math <math@8c818b55-100c-0410-84f9-a5579b780be3>
2006-11-16 14:40:47 +0000

    Add the copyright notices and the text of the GPL header

--- impl/sai_cas/gen_services/EditingServicesBindingImpl.java
+++ impl/sai_cas/gen_services/EditingServicesBindingImpl.java
@@ -1,9 +1,26 @@
-/**
- * EditingServicesBindingImpl.java
- *
- * This file was auto-generated from WSDL
- * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
- */
+/*
+	   Copyright (C) 2005-2006 Sergey Koposov
+   
+    Author: Sergey Koposov
+    Email: math@sai.msu.ru 
+    http://lnfm1.sai.msu.ru/~math
+
+    This file is part of SAI CAS
+
+    SAI CAS is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    SAI CAS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SAI CAS; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 package sai_cas.gen_services;
 import sai_cas.services.EditingServices;
--- src/sai_cas/services/UserServices.java
+++ src/sai_cas/services/UserServices.java
@@ -1,3 +1,29 @@
+/*
+	   Copyright (C) 2005-2006 Sergey Koposov
+   
+    Author: Sergey Koposov
+    Email: math@sai.msu.ru 
+    http://lnfm1.sai.msu.ru/~math
+
+    This file is part of SAI CAS
+
+    SAI CAS is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    SAI CAS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SAI CAS; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
 package sai_cas.services;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -10,7 +36,7 @@ import sai_cas.db.DBInterface;
 
 public class UserServices 
 {
-	static Logger logger = Logger.getLogger("sai_cas.MainAxisServices");
+	static Logger logger = Logger.getLogger("sai_cas.UserServices");
 	
 	public static void createNewUser(String adminLogin,
 			String adminPassword, String newLogin, String newPassword,
