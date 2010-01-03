6445530e75c0db7155300aefd83fc3b5391743f3
Sergey Koposov <math@fortune.mpia-hd.mpg.de>
2010-01-03 12:02:43 +0100

    ow the Axis2 services almost work (the only not working thing is the retrieval of the JDBC connection): I've put back the newly generated skeleton And I removed the apparently unnecessary BindingImpl...

---
+++ impl/sai_cas/gen_services/Sai_casSkeleton.java
@@ -0,0 +1,251 @@
+
+/**
+ * Sai_casSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.5.1  Built on : Oct 19, 2009 (10:59:00 EDT)
+ */
+    package sai_cas.gen_services;
+import sai_cas.services.MainAxisServices;
+import org.apache.log4j.Logger;
+
+    /**
+     *  Sai_casSkeleton java skeleton for the axisService
+     */
+    public class Sai_casSkeleton{
+	static Logger logger = Logger.getLogger("sai_cas.AXIS_SERVICES");
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getCatalogInfo
+         */
+        
+                 public sai_cas.gen_services.GetCatalogInfoResponse getCatalogInfo
+                  (
+                  sai_cas.gen_services.GetCatalogInfo getCatalogInfo
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getCatalogInfo");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getCatalogDescription
+         */
+        
+                 public sai_cas.gen_services.GetCatalogDescriptionResponse getCatalogDescription
+                  (
+                  sai_cas.gen_services.GetCatalogDescription getCatalogDescription
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getCatalogDescription");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+         */
+        
+                 public sai_cas.gen_services.GetCatalogNamesResponse getCatalogNames
+                  (
+                  
+                  )
+            {
+		logger.info("Running GetCatalogNames()");
+
+                sai_cas.gen_services.GetCatalogNamesResponse xx = new
+                sai_cas.gen_services.GetCatalogNamesResponse();
+                xx.set_return(MainAxisServices.getCatalogNames());
+                return xx;
+                
+                
+                //TODO : fill this with the necessary business logic
+                //throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getCatalogNames");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getColumnUnits
+         */
+        
+                 public sai_cas.gen_services.GetColumnUnitsResponse getColumnUnits
+                  (
+                  sai_cas.gen_services.GetColumnUnits getColumnUnits
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getColumnUnits");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getIndexes
+             * @throws SAICASFaultException : 
+         */
+        
+                 public sai_cas.gen_services.GetIndexesResponse getIndexes
+                  (
+                  sai_cas.gen_services.GetIndexes getIndexes
+                  )
+            throws SAICASFaultException{
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getIndexes");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getColumnDescriptions
+         */
+        
+                 public sai_cas.gen_services.GetColumnDescriptionsResponse getColumnDescriptions
+                  (
+                  sai_cas.gen_services.GetColumnDescriptions getColumnDescriptions
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getColumnDescriptions");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getColumnInfos
+         */
+        
+                 public sai_cas.gen_services.GetColumnInfosResponse getColumnInfos
+                  (
+                  sai_cas.gen_services.GetColumnInfos getColumnInfos
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getColumnInfos");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getColumnNames
+         */
+        
+                 public sai_cas.gen_services.GetColumnNamesResponse getColumnNames
+                  (
+                  sai_cas.gen_services.GetColumnNames getColumnNames
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getColumnNames");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getConeSearchAsString1
+         */
+        
+                 public sai_cas.gen_services.GetConeSearchAsString1Response getConeSearchAsString1
+                  (
+                  sai_cas.gen_services.GetConeSearchAsString1 getConeSearchAsString1
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getConeSearchAsString1");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getConeSearchAsString2
+         */
+        
+                 public sai_cas.gen_services.GetConeSearchAsString2Response getConeSearchAsString2
+                  (
+                  sai_cas.gen_services.GetConeSearchAsString2 getConeSearchAsString2
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getConeSearchAsString2");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getColumnUCDs
+             * @throws SAICASFaultException : 
+         */
+        
+                 public sai_cas.gen_services.GetColumnUCDsResponse getColumnUCDs
+                  (
+                  sai_cas.gen_services.GetColumnUCDs getColumnUCDs
+                  )
+            throws SAICASFaultException{
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getColumnUCDs");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getConeSearchAsString
+         */
+        
+                 public sai_cas.gen_services.GetConeSearchAsStringResponse getConeSearchAsString
+                  (
+                  sai_cas.gen_services.GetConeSearchAsString getConeSearchAsString
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getConeSearchAsString");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param getTableNames
+         */
+        
+                 public sai_cas.gen_services.GetTableNamesResponse getTableNames
+                  (
+                  sai_cas.gen_services.GetTableNames getTableNames
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getTableNames");
+        }
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+         */
+        
+                 public sai_cas.gen_services.GetDBLastChangedDateResponse getDBLastChangedDate
+                  (
+                  
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getDBLastChangedDate");
+        }
+     
+    }
+    
